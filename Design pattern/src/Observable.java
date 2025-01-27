50c43aa6f902c9d559ae8e56d594208c1487854b
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:54:56 +0100

    Documentation Observable.java

--- Design pattern/src/Observable.java
+++ Design pattern/src/Observable.java
@@ -1,5 +1,22 @@
+/**
+ * L'interface Observable représente un objet qui peut être observé par des observateurs.
+ * Elle permet d'ajouter, de retirer et de notifier des observateurs.
+ */
 public interface Observable {
+    /**
+     * Ajoute un observateur à la liste des observateurs.
+     * @param observer L'observateur à ajouter.
+     */
     public void ajouterObservateur(Observateur observer);
+
+    /**
+     * Retire un observateur de la liste des observateurs.
+     * @param observer L'observateur à retirer.
+     */
     public void retirerObservateur(Observateur observer);
+
+    /**
+     * Notifie tous les observateurs des changements.
+     */
     public void notifierObservateur();
 }

90204e587ec60949a23d5b4bb40bc2b40c514689
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 11:03:06 +0100

    Documentation RouteDesAlizes.java

--- Design pattern/src/RouteDesAlizes.java
+++ Design pattern/src/RouteDesAlizes.java
@@ -1,4 +1,12 @@
+/**
+ * La classe RouteDesAlizes implémente l'interface SuivreRoute.
+ * Elle représente une stratégie spécifique de suivi de route appelée "Route des Alizés".
+ */
 public class RouteDesAlizes implements SuivreRoute{
+    /**
+     * Méthode pour suivre la route selon la stratégie "Route des Alizés".
+     * Affiche "Route des Alizés" à la console.
+     */
     @Override
     public void suivreRoute() {
         System.out.println("Route des Alizés");

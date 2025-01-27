9a0f8c7b3ac3048b82092a25152c4f4d6a2397f5
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:55:23 +0100

    Documentation Observateur.java

--- Design pattern/src/Observateur.java
+++ Design pattern/src/Observateur.java
@@ -1,3 +1,11 @@
+/**
+ * L'interface Observateur représente un observateur dans le modèle Observer.
+ * Elle définit une méthode pour actualiser l'état de l'observateur lorsque l'objet observable change.
+ */
 public interface Observateur {
+    /**
+     * Méthode appelée pour actualiser l'état de l'observateur en réponse à un changement dans l'objet observable.
+     * @param observable L'objet observable qui a changé.
+     */
     public void actualise(Observable observable);
 }

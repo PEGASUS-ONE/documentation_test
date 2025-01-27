7b94862dbc37c45b719eb9cea41a22bca1db974a
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:54:13 +0100

    Documentation InscriptionMonocoque.java

--- Design pattern/src/InscriptionMonocoque.java
+++ Design pattern/src/InscriptionMonocoque.java
@@ -1,5 +1,16 @@
+/**
+ * La classe InscriptionMonocoque est une sous-classe d'InscriptionVoilier.
+ * Elle implémente la méthode factoryMethod pour créer des voiliers de type Monocoque.
+ */
 public class InscriptionMonocoque extends InscriptionVoilier{
 
+    /**
+     * Implémente la méthode factoryMethod pour créer un voilier spécifique en fonction de la classe du voilier.
+     * @param nomVoilier Le nom du voilier.
+     * @param nomSkipper Le nom du skipper du voilier.
+     * @param voilierClass La classe du voilier.
+     * @return Une instance de Voilier correspondant à la classe spécifiée.
+     */
     @Override
     public Voilier factoryMethod(String nomVoilier, String nomSkipper, VoilierClass voilierClass){
         switch(voilierClass){
@@ -11,7 +22,9 @@ public class InscriptionMonocoque extends InscriptionVoilier{
 
             case RhumMono:
                 return new RhumMono(nomVoilier, nomSkipper);
+
+            default:
+                return null;
         }
-        return null;
     }
 }

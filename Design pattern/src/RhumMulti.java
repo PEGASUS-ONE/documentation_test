0aabb12bd153c5957d1fdfe9a3486d739c6f8281
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:45:31 +0100

    documentation RhumMulti.java

--- Design pattern/src/RhumMulti.java
+++ Design pattern/src/RhumMulti.java
@@ -1,19 +1,40 @@
+/**
+ * La classe RhumMulti est une sous-classe de Multicoque représentant un type spécifique de bateau.
+ * Elle fournit plusieurs constructeurs pour créer des instances de RhumMulti avec des caractéristiques différentes.
+ */
 public class RhumMulti extends Multicoque{
+
+    /**
+     * Constructeur par défaut. 
+     * Crée une instance de RhumMulti sans nom.
+     */
     public RhumMulti(){}
 
+    /**
+     * Constructeur avec nom. 
+     * @param nom Le nom du RhumMulti.
+     */
     public RhumMulti(String nom){
         super(nom);
     }
 
+    /**
+     * Constructeur avec nom et nom du skipper.
+     * @param nom Le nom du RhumMulti.
+     * @param nomSkipper Le nom du skipper du RhumMulti.
+     */
     public RhumMulti(String nom, String nomSkipper){
         super(nom, nomSkipper);
     }
 
+    /**
+     * Constructeur avec nom et stratégie de suivi de route.
+     * @param nom Le nom du RhumMulti.
+     * @param suivreRoute La stratégie de suivi de route pour le RhumMulti.
+     */
     public RhumMulti(String nom, SuivreRoute suivreRoute){
         super(nom, suivreRoute);
     }
 
-    public RhumMulti(String nom, String nomSkipper, SuivreRoute suivreRoute){
-        super(nom, nomSkipper, suivreRoute);
-    }
-}
+    /**
+     * Construct

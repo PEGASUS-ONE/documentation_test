eaf8a0542299ff39a8931925bcf6ad94d0b34eb1
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:50:23 +0100

    Doumentation SecondeBarre.java

--- Design pattern/src/SecondeBarre.java
+++ Design pattern/src/SecondeBarre.java
@@ -1,10 +1,21 @@
+/**
+ * La classe SecondeBarre est une sous-classe d'ArmementSupplementaire.
+ * Elle ajoute une seconde barre comme armement supplémentaire pour un voilier.
+ */
 public class SecondeBarre extends ArmementSupplementaire {
 
+    /**
+     * Constructeur avec un voilier à décorer.
+     * @param voilierADecorer Le voilier à décorer.
+     */
     public SecondeBarre(Voilier voilierADecorer) {
         super(voilierADecorer);
     }
 
-
+    /**
+     * Retourne une représentation textuelle du voilier décoré avec l'armement supplémentaire.
+     * @return Une chaîne de caractères représentant le voilier décoré avec une seconde barre comme armement supplémentaire.
+     */
     @Override
     public String toString() {
         return super.toString() + "\n -Armement supplémentaire: Seconde barre";

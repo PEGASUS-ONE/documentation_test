8f6be5f480368ede7add075d57e9a6700fcdac3b
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 10:49:52 +0100

    Documentation Velo.java

--- Design pattern/src/Velo.java
+++ Design pattern/src/Velo.java
@@ -1,10 +1,21 @@
+/**
+ * La classe Velo est une sous-classe d'ArmementSupplementaire.
+ * Elle ajoute la fonctionnalité d'un vélo comme armement supplémentaire pour un voilier.
+ */
 public class Velo extends ArmementSupplementaire {
 
+    /**
+     * Constructeur avec un voilier à décorer.
+     * @param voilierADecorer Le voilier à décorer.
+     */
     public Velo(Voilier voilierADecorer) {
         super(voilierADecorer);
     }
 
-
+    /**
+     * Retourne une représentation textuelle du voilier décoré avec l'armement supplémentaire.
+     * @return Une chaîne de caractères représentant le voilier décoré avec un vélo comme armement supplémentaire.
+     */
     @Override
     public String toString() {
         return super.toString() + "\n -Armement supplémentaire: Velo";

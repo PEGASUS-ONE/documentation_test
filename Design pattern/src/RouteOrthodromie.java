494ddbcfda9fdc8bfca1a0868c7de051ce3fc86f
Valérian Lambert <154215783+PEGASUS-ONE@users.noreply.github.com>
2025-01-27 11:03:39 +0100

    Documentation RouteOrthodromie.java

--- Design pattern/src/RouteOrthodromie.java
+++ Design pattern/src/RouteOrthodromie.java
@@ -1,4 +1,12 @@
+/**
+ * La classe RouteOrthodromie implémente l'interface SuivreRoute.
+ * Elle représente une stratégie spécifique de suivi de route appelée "Route Orthodromie".
+ */
 public class RouteOrthodromie implements SuivreRoute{
+    /**
+     * Méthode pour suivre la route selon la stratégie "Route Orthodromie".
+     * Affiche "Route Orthodromie" à la console.
+     */
     @Override
     public void suivreRoute() {
         System.out.println("Route Orthodromie");
